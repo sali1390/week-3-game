f00fb013ad04d038e0ff49a56737af77320737c3
Saleema Ali <sali1390@gmail.com>
2016-10-19 23:52:42 -0400

    Added more styling and further cleaned up JS file

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,46 +11,84 @@ window.onload = function () {
 		return compPick;
 	};
 
-	function displayDashes(word) {
-		var display = "";
-		for (var i = 0; i < word.length; i++)
-			{
-				display = display + "-";
-			}
-			return display;
-	};
+	// function displayDashes(word) {
+	// 	var display = "";
+	// 	for (var i = 0; i < word.length; i++)
+	// 		{
+	// 			display = display + "-";
+	// 		}
+	// 		return display;
+	// 
 
-	function isLetterInWord(word, letter) {
-		if (word.search(letter)!= -1) {
-			var index = word.search(letter);
-			var newDisplay = display.substr(0, index) + letter + display.substr(index + 1);
-			guessesLeft--;
-		}
-		else {
-			newDisplay = display;
-			console.log("Display: " + display);
-			guessesLeft--;
-			console.log(guessesLeft);
-		}
-		return newDisplay;
+	function displayDashes (word , guesses) {
+		var display = "" ;
+		for ( var i = 0 ; i < word. length ; i++) {
+			if ( inArray (word[i] , guesses))
+			display = display + word[i] ;
+			else
+			display = display + "-" ;
+			}
+		guessesLeft--;
+		return display ;
+	}
+
+	function inArray (myElement , myArray) {
+		for ( var i = 0 ; i < myArray. length ; i++) {
+			if (myArray[i] == myElement)
+			return true
+			}
+		return false ;
+	}
+
+	// function isLetterInWord(word, letter) {
+	// 	if (word.search(letter)!= -1) {
+	// 		var index = word.search(letter);
+	// 		var newDisplay = display.substr(0, index) + letter + display.substr(index + 1);
+	// 		guessesLeft--;
+	// 	}
+	// 	else {
+	// 		newDisplay = display;
+	// 		guessesLeft--;
+	// 		console.log(guessesLeft);
+	// 	}
+	// 	return newDisplay;
+	// }
+
+	function resetGame() {
+		userGuesses = [];
+		guessesLeft = 13;
+		var word = pickWord();
+		var display = displayDashes(word, userGuesses);
+		document.getElementById("dashes").innerHTML = display;
 	}
 
 	wins = 0;
 	userGuesses = [];
 	guessesLeft = 13;	
 	var word = pickWord();
-	var display = displayDashes(word);
+	var display = displayDashes(word, userGuesses);
 	document.getElementById("dashes").innerHTML = display;
 	
-	console.log(word)
+	console.log(word);
 
 	document.onkeydown = function (event) {
 		document.getElementById("wins").innerHTML = wins;
 		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 		userGuesses.push(userInput);
-		display = isLetterInWord(word, userInput);
-		document.getElementById("dashes").innerHTML = display;
+		// display = isLetterInWord(word, userInput);
+		document.getElementById("dashes").innerHTML = displayDashes(word, userGuesses);
 		document.getElementById("guessesLeft").innerHTML = guessesLeft;
 		document.getElementById("userGuesses").innerHTML = userGuesses + " ";
-	};
+		if (display == word) {
+			playAgain = alert("You Win! Would you like to play again?");
+			if (playAgain == true) {
+				resetGame();
+			}
+		} else if (guessesLeft == 0) {
+			playAgain = alert("You Lose. Would you like to play again?");
+			if (playAgain == true) {
+				resetGame();
+			}
+		}
+	}
 }
