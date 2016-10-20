3175f96356c33aaf208b5e6d5aed94f6e1fe404c
Saleema Ali <sali1390@gmail.com>
2016-10-19 23:11:40 -0400

    Added code for tallying of guesses

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 
 	var wins;
-	var userGuesses = [ ];
+	var userGuesses = [];
 	var guessesLeft;
 
 	function pickWord() {
@@ -24,6 +24,7 @@ window.onload = function () {
 		if (word.search(letter)!= -1) {
 			var index = word.search(letter);
 			var newDisplay = display.substr(0, index) + letter + display.substr(index + 1);
+			guessesLeft--;
 		}
 		else {
 			newDisplay = display;
@@ -35,11 +36,8 @@ window.onload = function () {
 	}
 
 	wins = 0;
-	document.getElementById("wins").innerHTML = wins;
 	userGuesses = [];
-	document.getElementById("userGuesses").innerHTML = userGuesses;
-	guessesLeft = 13;
-	document.getElementById("guessesLeft").innerHTML = guessesLeft;	
+	guessesLeft = 13;	
 	var word = pickWord();
 	var display = displayDashes(word);
 	document.getElementById("dashes").innerHTML = display;
@@ -47,10 +45,12 @@ window.onload = function () {
 	console.log(word)
 
 	document.onkeydown = function (event) {
+		document.getElementById("wins").innerHTML = wins;
 		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 		userGuesses.push(userInput);
 		display = isLetterInWord(word, userInput);
+		document.getElementById("dashes").innerHTML = display;
+		document.getElementById("guessesLeft").innerHTML = guessesLeft;
 		document.getElementById("userGuesses").innerHTML = userGuesses + " ";
-		
 	};
 }
